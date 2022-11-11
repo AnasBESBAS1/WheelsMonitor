c4bd26c74b1bcfb710799b44eaea1897b12ca98b
anasbesbas1 <anas.besbas9@gmail.com>
2022-11-12 00:06:47 +0100

    code optimasation && toolbarcolor dynamic fun added && global biding works

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from "@angular/core";
 import { Router, NavigationExtras } from '@angular/router';
 import { WheelRepositoryService } from "../services/wheel-repository.service";
 
-
 export interface Wheel {
   pressure: number;
   wear: number;
@@ -17,16 +16,33 @@ export interface Wheel {
 
 export class HomePage implements OnInit {
 
-  toolbarcolor = "medium";
+
   rearLeft = "Rear Left";
   rearRight = "Rear right";
   frontLeft = "Front Left";
   frontRight = "Front right";
   coloring = "";
+  toolbarcolor = "";
+
   @Input() wheelArray! : Array<Wheel>;
 
-  constructor(private router: Router, private wheelRepositoryService : WheelRepositoryService ) {}
+  constructor(private router: Router, private wheelRepositoryService : WheelRepositoryService ) {
+  }
 
+  ngOnInit() {
+    this.wheelArray = this.wheelRepositoryService._wheels;
+  }
+
+  setColorBarGlob(){
+    for (var i = 0 ; i< 4 ; ++i){
+      if(this.wheelArray[i].pressure > 3 || this.wheelArray[i].pressure < 2.5 || this.wheelArray[i].wear > 20000){
+        this.toolbarcolor = "danger";
+        return;
+      }
+    }
+    this.toolbarcolor = "medium";
+    return;
+  }
   setColor(_presseure, _wear){
     if (_presseure > 3 || _presseure < 2.5) {
       this.coloring = "red";
@@ -40,21 +56,16 @@ export class HomePage implements OnInit {
 
   checkStatus(_presseure, _wear): string {
     if (_presseure > 3 || _presseure < 2.5) {
-      this.toolbarcolor = "danger";
       return "Warning:presseure";
     } else if (_wear > 20000) {
-      this.toolbarcolor = "danger";
       return "Warning:wear";
     }
-    this.toolbarcolor = "medium";
     return "status : ok";
   }
-  ngOnInit() {
-    this.wheelArray = this.wheelRepositoryService._wheels;
-  }
+
   openDetailsWithState(nameofwheel,index) {
     this.router.navigate(['loader'], { state: { _name: nameofwheel,
     _index : index,
-  } });
+  }});
     }  //https://ionicacademy.com/pass-data-angular-router-ionic-4/
 }
--- src/app/pages/loader/loader.page.ts
+++ src/app/pages/loader/loader.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { WheelRepositoryService } from "../../services/wheel-repository.service";
 import { Wheel } from "../../data/wheel";
+import { AppLauncher } from '@capacitor/app-launcher';
 
 @Component({
   selector: 'app-loader',
@@ -16,14 +17,29 @@ export class LoaderPage implements OnInit {
   index : number;
   @Input() wheelArray! : Array<Wheel>;
 
+  openMaps = async () => {
+    this.name = "anas";
+    await AppLauncher.openUrl({url:'com.google.android.apps.maps'});
+  };
 
-  constructor(private router: Router, private route: ActivatedRoute,private wheelRepositoryService : WheelRepositoryService ) {
+  constructor(private router: Router, private route: ActivatedRoute,
+        private wheelRepositoryService : WheelRepositoryService ) {
    this.route.params.subscribe(params => {
      this.name = router.getCurrentNavigation().extras.state._name;
      this.index = router.getCurrentNavigation().extras.state._index;
    });
  }
 
+   launchMaps(){
+    const checkCanOpenUrl = async () => {
+      this.name = "anas";
+      const { value } = await AppLauncher.canOpenUrl({ url: 'com.google.android.apps.maps' });
+
+      console.log('Can open url: ', value);
+    };
+
+    checkCanOpenUrl();
+  }
   ngOnInit() {
     this.wheelArray = this.wheelRepositoryService._wheels;
   }
